df08539fa4bd085679dce1de74787f3eb9c03a5c
Florent Morselli <florent.morselli@spomky-labs.com>
2024-11-08 12:00:47 +0100

    Update Symfony Stimulus-bundle and adjust import order
    Upgraded Symfony Stimulus-bundle to version 2.21 from 2.13. Adjusted import order in `assets/app.js` to align with the updated bundle configuration.

--- assets/app.js
+++ assets/app.js
@@ -1,7 +1,7 @@
+import './bootstrap.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import 'swiper/swiper-bundle.min.css';
 import './styles/app.css';
 
-import './bootstrap.js';
 import './transitions.js';
